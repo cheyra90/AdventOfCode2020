afd373801d28a37dfe9a5a8c8cbe42acaa8a169a
unknown <cheyne.ravenscroft@gmail.com>
2020-12-23 09:13:15 +0000

    Done day 1

---
+++ 1/2.py
@@ -0,0 +1,15 @@
+from itertools import *
+import numpy as np
+
+fp = '1/input.txt'
+dat = open(fp).read().strip().split('\n')
+dat = [int(x) for x in dat]
+
+cmb = list(combinations(dat, 3)) # get all combinations of pairwise combos
+
+def is_valid(x):
+    return sum(x) == 2020 # Check for validity 
+
+ans = np.prod(list(filter(is_valid, cmb))) # apply the filter to the list of all combinations
+
+print(ans)
